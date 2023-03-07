5ecd787a282bf5dceefef282148546bf7feda206
daysant <92995911+daysantace@users.noreply.github.com>
2023-03-07 22:47:38 +0000

    utter bloat

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.use(express.static(__dirname), (_, res, next) => {
-  res.status(404)
-  res.sendFile(__dirname + "/404/404.html")
-});
-app.listen(8080);
